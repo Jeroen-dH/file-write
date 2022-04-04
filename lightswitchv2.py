5a27b4ec790e0df17d9fd1576ea5a09b74aedb02
Jeroen <99068895@Mydavici.nl>
2022-04-04 13:11:43 +0200

    file log done

--- lightswitchv2.py
+++ lightswitchv2.py
@@ -10,13 +10,11 @@ def knop(a):
     file = open (r"C:\Users\Jeroen\Desktop\Projecten\file-write\actions.log", "a")
     global button, count
     if count == 0:
-        print("licht aan.")
         file.writelines("\nLicht aan")
         button.config(text='Light on.', bg="white", fg='Black')
         window.config(bg="yellow")
         count += 1
     else:
-        print("licht uit.")
         file.writelines("\nLicht uit")
         button.config(text='Light off.', bg='white', fg='black')
         window.config(bg="black")
