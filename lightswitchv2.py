0aee04947dfc960c52a1da7766209be050402b4d
Jeroen-DH <99068895@mydavinci.nl>
2022-04-06 21:39:48 +0200

    auto log done

--- lightswitchv2.py
+++ lightswitchv2.py
@@ -4,32 +4,33 @@ count = 0
 a = 0
 check = True
     # schijf hier tussen je code
-try:
-    with open(r"C:\Users\Jeroen\Desktop\Projecten\file-write\actions.log", "x") as file:
-        file.writelines("\nNew session.")
-except:
-    file = open (r"C:\Users\Jeroen\Desktop\Projecten\file-write\actions.log", "a")
-    if check:
-        file.writelines("\nNew session.")
-        check = False
+switchbutton = tk.Button(text='light off.', bg="white", fg="black",command=lambda: [knop(a)])
+window.config(bg='black')
+switchbutton.pack(pady = 50, padx = 60)
+
 def knop(a):
-    global button, count,check,file
+    global switchbutton, count,check,file
+    try:
+        file = open(r"actions.log","a")
+        if check:
+            file.writelines("\nNew session.")
+            check = False
+    except:
+        with open(r"actions.log", "x") as file:
+            file.writelines("New session.")
     if count == 0:
         file.writelines("\nLicht aan")
-        button.config(text='Light on.', bg="white", fg='Black')
+        switchbutton.config(text='Light on.', bg="white", fg='Black')
         window.config(bg="yellow")
         count += 1
     else:
         file.writelines("\nLicht uit")
-        button.config(text='Light off.', bg='white', fg='black')
+        switchbutton.config(text='Light off.', bg='white', fg='black')
         window.config(bg="black")
         count -= 1
     file.close()
 
-button = tk.Button(text='light off.', bg="white", fg="black")
-window.config(bg='black')
-button.pack(pady = 50, padx = 60)
-button.bind('<Button>', knop)
+
 # schijf hier tussen je code
 
 window.mainloop()
